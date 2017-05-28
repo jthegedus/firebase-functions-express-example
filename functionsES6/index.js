2f41863615221098597a62a2ac6ebd83e599c252
Unknown <jthegedus@jttech.io>
2017-05-29 00:38:06 +1000

    Add comments. Catchall Express route.

--- functionsES6/index.js
+++ functionsES6/index.js
@@ -2,14 +2,19 @@ import * as functions from "firebase-functions"
 import express from "express"
 
 const app = express()
-
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.send("Hello from Express on Firebase!")
 })
 
-export let helloWorld = functions.https.onRequest((req, res) => {
+// requires trailing slash '/' be appended on call, like so
+// before: us-central1-<project-name>.cloud-functions.net/helloWorld
+// after:  us-central1-<project-name>.cloud-functions.net/helloWorld/
+export let addSlash = functions.https.onRequest(app)
+
+// no '/' required to call correctly
+export let noSlash = functions.https.onRequest((req, res) => {
   if (!req.path) {
-    req.url = `/${req.url}` // prepend to keep query params
+    req.url = `/${req.url}` // prepend '/' to keep query params
   }
   return app(req, res)
 })
