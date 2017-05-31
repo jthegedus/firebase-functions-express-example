996133a87aefb139df54c3185f73300d078390c8
Unknown <jthegedus@jttech.io>
2017-05-31 15:41:30 +1000

    Reimplement examples to match blog post's gists

--- functionsES6/index.js
+++ functionsES6/index.js
@@ -3,36 +3,34 @@ import cors from "cors"
 import express from "express"
 
 /* Express */
-const app = express()
-app.get("*", (request, response) => {
-  response.send("Hello from Express on Firebase!")
+const app1 = express()
+app1.get("*", (req, res) => {
+  res.send("Hello from Express on Firebase!")
 })
 
-// requires trailing slash '/' be appended on call, like so
-// before: us-central1-<project-name>.cloud-functions.net/helloWorld
-// after:  us-central1-<project-name>.cloud-functions.net/helloWorld/
-export let requriesSlash = functions.https.onRequest(app)
+export let api1 = functions.https.onRequest(app1)
 
-// no '/' required to call correctly
-export let noSlash = functions.https.onRequest((request, response) => {
+/* Express with CORS */
+const app2 = express()
+app2.use(cors({ origin: true }))
+app2.get("*", (request, response) => {
+  response.send("Hello from Express on Firebase with CORS!")
+})
+export let api2 = functions.https.onRequest(app2)
+
+/* Express with CORS & automatic trailing '/' solution */
+const app3 = express()
+app3.use(cors({ origin: true }))
+app3.get("*", (request, response) => {
+  response.send(
+    "Hello from Express on Firebase with CORS! No trailing '/' required!"
+  )
+})
+
+// not as clean, but a better endpoint to consume
+export let api3 = functions.https.onRequest((request, response) => {
   if (!request.path) {
     request.url = `/${request.url}` // prepend '/' to keep query params if any
   }
-  return app(request, response)
-})
-
-/* With CORS */
-const appWithCors = express()
-appWithCors.use(cors({ origin: true }))
-appWithCors.get("*", (request, response) => {
-  response.send("Hello from Express with CORS on Firebase!")
-})
-
-export let requiresSlashUsingCors = functions.https.onRequest(appWithCors)
-
-export let noSlashUsingCors = functions.https.onRequest((request, response) => {
-  if (!request.path) {
-    request.url = `/${request.url}` // prepend '/' to keep query params if any
-  }
-  return appWithCors(request, response)
+  return app3(request, response)
 })
